bcd15eab0dfb07fc0e1c44cc5439b90c0d26abfe
Maeng JuYoung <wndud6302@naver.com>
2024-12-30 13:35:50 +0900

    query 초기 세팅

---
+++ query_test/Source/Page/TE1000.js
@@ -0,0 +1,53 @@
+
+TE1000 = class TE1000 extends AView
+{
+	constructor()
+	{
+		super()
+
+		this.contiKey = '';
+	}
+
+	init(context, evtListener)
+	{
+		super.init(context, evtListener)
+
+		this.createCkEditor(this.noticeContent.element);
+	}
+
+	onInitDone()
+	{
+		super.onInitDone()
+
+		//TODO:edit here
+
+	}
+
+	onActiveDone(isFirst)
+	{
+		super.onActiveDone(isFirst)
+
+		//TODO:edit here
+
+	}
+
+    createCkEditor(target)
+    {
+        return ClassicEditor.create(target, {
+            language: 'ko',
+            extraPlugins: [customUploadAdapterPlugin],
+        })
+        .then(editor => {
+            editor.editing.view.change(writer => writer.setStyle('height', '200px', editor.editing.view.document.getRoot()))
+            this.noticeContent = editor;
+        })
+        .catch(console.error);
+
+        function customUploadAdapterPlugin(editor) {
+            editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+                return new UploadAdapter(loader, `${config.SERVER_ADDRESS}:${config.SERVER_PORT}/upload`);
+            };
+        }
+    }
+}
+
