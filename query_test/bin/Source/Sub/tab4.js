573d8c3ea10b0aff98afac12d3c6619560555886
Maeng JuYoung <wndud6302@naver.com>
2024-12-31 17:28:15 +0900

    TE2000 레이아웃 수정중

---
+++ query_test/bin/Source/Sub/tab4.js
@@ -0,0 +1,37 @@
+
+tab4 = class tab4 extends AView
+{
+	constructor()
+	{
+		super()
+
+		//TODO:edit here
+
+	}
+
+	init(context, evtListener)
+	{
+		super.init(context, evtListener)
+
+		//TODO:edit here
+
+	}
+
+	onInitDone()
+	{
+		super.onInitDone()
+
+		//TODO:edit here
+
+	}
+
+	onActiveDone(isFirst)
+	{
+		super.onActiveDone(isFirst)
+
+		//TODO:edit here
+
+	}
+
+}
+
