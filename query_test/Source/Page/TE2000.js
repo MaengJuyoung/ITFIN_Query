85b308d24bad6484480f8bd51bf895286c7172e0
Maeng JuYoung <wndud6302@naver.com>
2025-01-08 09:51:21 +0900

    코드 수정 완료

--- query_test/Source/Page/TE2000.js
+++ query_test/Source/Page/TE2000.js
@@ -109,13 +109,13 @@ TE2000 = class TE2000 extends AView
         );
     }
 
-    // 조회 버튼 클릭 시 
+    // 회원 조회 버튼 클릭 시 
 	onInsertBtnClick(comp, info, e)
 	{
         this.loadUserGrid();
 	}
 
-    // 다음 버튼 클릭 시 
+    // 회원 다음 버튼 클릭 시 
 	onContiKeyClick(comp, info, e)
 	{
         this.loadUserGrid(this.contiKey);
@@ -129,10 +129,16 @@ TE2000 = class TE2000 extends AView
         if (index == -1) return;
 
         const acnt_cd = thisObj.grid.getDataByOption(info)[3];        
-        thisObj.acnt_cd = acnt_cd;          // 선택한 회원의 계좌번호 전역변수에 저장
-        this.innerHeader.element.style.display = 'flex';
 
-        thisObj.tab.selectTabById('tab1');
+        // 선택한 회원의 계좌번호 전역변수에 저장
+        thisObj.acnt_cd = acnt_cd;                  
+        
+        // 선택된 탭이 있다면 탭 그리드 로드
+        const initTab = this.tab.getSelectedView();
+        if (initTab) {
+            thisObj.ordAction.selectBtnByValue(0);
+            this.loadTabGrid();
+        }
 	}
 
     // tab 선택 시 - TE3000, TE3010, TE3020, TE3030
@@ -140,51 +146,38 @@ TE2000 = class TE2000 extends AView
 	{
         const thisObj = this;
         thisObj.tabContiKey = '';       // tab contiKey 초기화 
+        thisObj.innerHeader.element.style.display = 'flex';
 
+        // 선택된 버튼의 아이디를 가져와서 해당 탭 보여주기
         const tabId = comp.compId;
         thisObj.tab.selectTabById(tabId);
-
         
-        if (tabId != 'tab1') this.ordAction.element.style.display = 'none';
-        else this.ordAction.element.style.display = 'block';
-/*
-        const initTab = thisObj.tab.getSelectedView();
-        const insertBtn = initTab.$ele[0].childNodes[0].acomp.$ele[0].childNodes[1];
-        const tabContikeyBtn = initTab.$ele[0].childNodes[0].acomp.$ele[0].childNodes[2];
-
-        console.log("initTab",initTab)
-        console.log("insertBtn",insertBtn)
-        console.log("tabContikeyBtn",tabContikeyBtn)
-*/
-
-
-/*
-        const queryMap = {       // 탭 ID와 매핑되는 쿼리 이름
-            tab1: 'TE3000',
-            tab2: 'TE3010',
-            tab3: 'TE3020',
-            tab4: 'TE3030',
-        };
+        // tab1 일 경우 radioGroup 보여주기 
+        if (tabId == 'tab1') {
+            thisObj.ordAction.element.style.display = 'block';
+            thisObj.ordAction.selectBtnByValue(0);
+        } else thisObj.ordAction.element.style.display = 'none';
         
-        if (tabId !== 'tab1'){
-            if (comp.selectedTab.view){
-                const initTab = comp.selectedTab.view;
-                console.log("initTab",initTab);
-                const tabGrid = initTab.grid;
-                const contiBtn = initTab.contiKey.$ele[0]
-                contiBtn.onClick()
-                tabGrid.removeAll();
-            }
-
-            
-        }*/
+        // 쿼리 실행을 위해 전역에 탭 저장 후 탭 그리드 로드 함수 호출
+        thisObj.selectedTab = tabId;
+        thisObj.loadTabGrid();
 	}
 
+    // 탭 구분 변경 시 contiKey 초기화 
+	onOrdActionChange(comp, info, e) { 
+        this.tabContiKey = '';	
+    }
+
+    // 탭 뷰 그리드에 데이터 조회 쿼리 
     loadTabGrid(tabContiKey=''){
         const thisObj = this;
-        if (!tabContiKey) thisObj.grid.removeAll();    // 그리드 초기화
         const tabId = thisObj.selectedTab;
-        console.log("tabId",tabId)
+
+        const initTab = thisObj.tab.getTabById(tabId);
+        if (initTab.content.view){
+            const tabGrid = initTab.content.view.grid;
+            if (!tabContiKey) tabGrid.removeAll();    // 그리드 초기화
+        }
 
         const queryMap = {       // 탭 ID와 매핑되는 쿼리 이름
             tab1: 'TE3000',
@@ -194,45 +187,39 @@ TE2000 = class TE2000 extends AView
         };
 
         theApp.qm.sendProcessByName(queryMap[tabId], this.getContainerId(), null,
-                function(queryData) { 
-                    const inblock1 = queryData.getBlockData('InBlock1')[0];
-                    inblock1.acnt_cd = thisObj.acnt_cd;
-                    inblock1.next_key = tabContiKey;  // 이전에 가져온 마지막 키를 전달
-                },
-                function(queryData) { 
-                    const errorData = this.getLastError();
-                    if (errorData.errFlag === 'E') {
-                        console.log('Error Data:', errorData);
-                        AToast.show(`${queryName} 실행 중 에러가 발생했습니다.`);
-                        return;
-                    }
-                    const outblock1 = queryData.getBlockData('OutBlock1');
-                    if (!outblock1 || outblock1.length <= 0) {
-                        AToast.show('조회된 데이터가 없습니다.');
-                        return;
-                    }
-                    thisObj.tabContiKey = outblock1[outblock1.length - 1].next_key;
-
+            function(queryData) { 
+                const inblock1 = queryData.getBlockData('InBlock1')[0];
+                inblock1.acnt_cd = thisObj.acnt_cd;
+                inblock1.next_key = tabContiKey;  // 이전에 가져온 마지막 키를 전달
+            },
+            function(queryData) { 
+                const errorData = this.getLastError();
+                if (errorData.errFlag === 'E') {
+                    console.log('Error Data:', errorData);
+                    AToast.show(`${queryName} 실행 중 에러가 발생했습니다.`);
+                    return;
                 }
-            );
+                const outblock1 = queryData.getBlockData('OutBlock1');
+                if (!outblock1 || outblock1.length <= 0) {
+                    AToast.show('조회된 데이터가 없습니다.');
+                    return;
+                }
+                thisObj.tabContiKey = outblock1[outblock1.length - 1].next_key;
+            }
+        );
     }
 
     // 탭 조회 버튼 클릭 시 
-	onTabContiKeyClick(comp, info, e)
+    onTabInsertBtnClick(comp, info, e)
 	{
-        const initTab = this.tab.getSelectedView().className;
-        console.log("initTab",initTab)
-        
-        //this.loadTabGrid();
+        this.loadTabGrid();
 	}
+	
 
     // 탭 다음 버튼 클릭 시 
-	onTabInsertBtnClick(comp, info, e)
+	onTabContiKeyClick(comp, info, e)
 	{
-        const initTab = this.tab.getSelectedView().className;
-        console.log("initTab",initTab)
-        this.selectedTab = initTab;
-        //this.loadTabGrid(this.tabContiKey);
+        this.loadTabGrid(this.tabContiKey);
 	}
 }
 
--- query_test/bin/Source/Sub/tab1.js
+++ query_test/bin/Source/Sub/tab1.js
@@ -1,68 +1,68 @@
 
-tab1 = class tab1 extends AView
-{
-	constructor()
-	{
-		super()
-        this.contiKey = '';
-        this.acnt_cd = '';
-	}
+// tab1 = class tab1 extends AView
+// {
+// 	constructor()
+// 	{
+// 		super()
+//         this.contiKey = '';
+//         this.acnt_cd = '';
+// 	}
 
-	init(context, evtListener)
-	{
-		super.init(context, evtListener)
-	}
+// 	init(context, evtListener)
+// 	{
+// 		super.init(context, evtListener)
+// 	}
 
-    onInitDone()
-	{
-		super.onInitDone()
-	}
+//     onInitDone()
+// 	{
+// 		super.onInitDone()
+// 	}
 
-	onActiveDone(isFirst)
-	{
-		super.onActiveDone(isFirst)
-	}
+// 	onActiveDone(isFirst)
+// 	{
+// 		super.onActiveDone(isFirst)
+// 	}
 
-    // 조회 버튼 클릭 시 
-	onTabInsertBtnClick(comp, info, e)
-	{
-        this.acnt_cd = this.getContainerView().acnt_cd  // 상위 컨테이너 뷰에서 계좌번호 가져오기 
+//     // 조회 버튼 클릭 시 
+// 	onTabInsertBtnClick(comp, info, e)
+// 	{
+//         this.acnt_cd = this.getContainerView().acnt_cd  // 상위 컨테이너 뷰에서 계좌번호 가져오기 
         
-        this.loadGrid();
-	}
+//         this.loadGrid();
+// 	}
 
-    // 다음 버튼 클릭 시 
-	onTabContiKeyClick(comp, info, e)
-	{
-		this.loadGrid(this.contiKey);
-	}
+//     // 다음 버튼 클릭 시 
+// 	onTabContiKeyClick(comp, info, e)
+// 	{
+// 		this.loadGrid(this.contiKey);
+// 	}
 
-    // 거래내역 조회 - TE3000
-    loadGrid(contiKey=''){
-        const thisObj = this;
-        if (!contiKey) thisObj.grid.removeAll();           // 그리드 초기화
+//     // 거래내역 조회 - TE3000
+//     loadGrid(contiKey=''){
+//         const thisObj = this;
+//         if (!contiKey) thisObj.grid.removeAll();           // 그리드 초기화
         
-        theApp.qm.sendProcessByName('TE3000', this.getContainerId(), null,
-            function(queryData) { 
-                const inblock1 = queryData.getBlockData('InBlock1')[0];
-                inblock1.acnt_cd = thisObj.acnt_cd;
-                inblock1.next_key = contiKey;  // 이전에 가져온 마지막 키를 전달
-            },
-            function(queryData) { 
-                const errorData = this.getLastError();
-                if (errorData.errFlag === 'E') {
-                    console.log('Error Data:', errorData);
-                    AToast.show(`${queryName} 실행 중 에러가 발생했습니다.`);
-                    return;
-                }
-                const outblock1 = queryData.getBlockData('OutBlock1');
-                if (!outblock1 || outblock1.length <= 0) {
-                    AToast.show('조회된 데이터가 없습니다.');
-                    return;
-                }
-                thisObj.contiKey = outblock1[outblock1.length - 1].next_key;
-            }
-        );
-    }
-}
+//         theApp.qm.sendProcessByName('TE3000', this.getContainerId(), null,
+//             function(queryData) { 
+//                 const inblock1 = queryData.getBlockData('InBlock1')[0];
+//                 inblock1.acnt_cd = thisObj.acnt_cd;
+//                 inblock1.next_key = contiKey;  // 이전에 가져온 마지막 키를 전달
+//             },
+//             function(queryData) { 
+//                 const errorData = this.getLastError();
+//                 if (errorData.errFlag === 'E') {
+//                     console.log('Error Data:', errorData);
+//                     AToast.show(`${queryName} 실행 중 에러가 발생했습니다.`);
+//                     return;
+//                 }
+//                 const outblock1 = queryData.getBlockData('OutBlock1');
+//                 if (!outblock1 || outblock1.length <= 0) {
+//                     AToast.show('조회된 데이터가 없습니다.');
+//                     return;
+//                 }
+//                 thisObj.contiKey = outblock1[outblock1.length - 1].next_key;
+//             }
+//         );
+//     }
+// }
 
